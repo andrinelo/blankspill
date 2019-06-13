aa8e32442ac06ec59a63b917997bed1d05541669
Andrine Lossius <andlossius@gmail.com>
2019-06-13 10:30:24 +0200

    code editor with react ace

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AceEditor from "react-ace";
+import 'brace/mode/javascript'
+import 'brace/theme/monokai'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <AceEditor
+      mode="javascript"
+      theme="monokai"
+     />
     </div>
   );
 }
