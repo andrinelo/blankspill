76284c1056d853a88e4d48a5713a571d8a84bb74
Andrine Lossius <andlossius@gmail.com>
2019-06-13 12:53:43 +0200

    console log with code editor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-import AceEditor from "react-ace";
-import 'brace/mode/javascript'
-import 'brace/theme/monokai'
+import brace from 'brace';
+import AceEditor from 'react-ace';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
 
+export default class App extends React.Component {
 
-function App() {
-  return (
-    <div> 
-      <AceEditor
-      mode="javascript"
-      theme="monokai"
-     />
-    </div>
-  );
+    constructor(props, context) {
+        super(props, context);
+        
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(newValue) {
+        console.log('change', newValue);
+    }
+
+    render() {
+        return (
+            <div>
+                <AceEditor
+                    mode="javascript"
+                    theme="monokai"
+                    onChange={this.onChange}
+                    name="UNIQUE_ID_OF_DIV"
+                    editorProps={{
+                        $blockScrolling: true
+                    }}
+                />
+            </div>
+        );
+    }
 }
-
-export default App;
