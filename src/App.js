729a1cfa71099eecc1c6f6d67eeebeb6a1d4be30
Ragnhild <ragnhildk98@gmail.com>
2019-06-14 13:48:04 +0200

    add run button

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
-import brace from 'brace';
-import AceEditor from 'react-ace';
-import 'brace/mode/javascript';
-import 'brace/theme/monokai';
+import React, { Component } from "react";
+import "./App.css";
+import brace from "brace";
+import AceEditor from "react-ace";
+import "brace/mode/javascript";
+import "brace/theme/monokai";
 
 export default class App extends React.Component {
-
-    constructor(props, context) {
-        super(props, context);
-        
-        this.onChange = this.onChange.bind(this);
+  constructor(props, context) {
+    super(props, context);
+    this.state = { aceEditorValue: "abc" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.aceEditorValue !== nextState.aceEditorValue) {
+      return false;
+    } else {
+      return true;
     }
+  }
+  handleChange = newValue => {
+    this.setState({ aceEditorValue: newValue });
+  };
 
-    onChange(newValue) {
-        console.log('change', newValue);
-    }
+  handleClick() {
+    console.log(this.state.aceEditorValue);
+  }
 
-    render() {
-        return (
-            <div>
-                <AceEditor
-                    mode="javascript"
-                    theme="monokai"
-                    onChange={this.onChange}
-                    name="UNIQUE_ID_OF_DIV"
-                    editorProps={{
-                        $blockScrolling: true
-                    }}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <AceEditor
+          //ref="editor" //test
+          mode="javascript"
+          theme="monokai"
+          onChange={this.handleChange}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{
+            $blockScrolling: true,
+          }}
+        />
+        <button className="button" onClick={this.handleClick}>
+          Run
+        </button>
+      </div>
+    );
+  }
 }
