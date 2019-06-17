896b485815917efe310ada866d03defe54abf7ca
Ragnhild <ragnhildk98@gmail.com>
2019-06-17 13:35:06 +0200

    .

--- src/App.js
+++ src/App.js
@@ -10,31 +10,38 @@ import "brace/theme/monokai";
 export default class App extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.state = { aceEditorValue: "ghj" };
+    this.state = { value: "5+7", button: false };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
   shouldComponentUpdate(nextState) {
-    if (this.state.aceEditorValue !== nextState.aceEditorValue) {
+    if (this.state.value !== nextState.value) {
       return false;
     } else {
       return true;
     }
   }
   handleChange = newValue => {
-    this.setState({ aceEditorValue: newValue });
+    this.setState({ value: newValue });
   };
 
   handleClick() {
-    console.log(eval(this.state.aceEditorValue));
+    this.setState({ button: !this.state.button });
+    console.log(eval(this.state.value));
   }
 
   render() {
     return (
       <div>
+        {/*
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=y7hVM8CFsGE"
+          playing
+      />*/}
         <AceEditor
           mode="javascript"
           theme="monokai"
+          value={this.state.value}
           onChange={this.handleChange}
           name="UNIQUE_ID_OF_DIV"
           editorProps={{
@@ -44,10 +51,7 @@ export default class App extends React.Component {
         <button className="button" onClick={this.handleClick}>
           Run
         </button>
-        <ReactPlayer
-          url="https://www.youtube.com/watch?v=y7hVM8CFsGE"
-          playing
-        />
+        <div>{eval(this.state.value)}</div>
       </div>
     );
   }
