2750ac8b3b2582627b2a9575e393771d7410814a
Ragnhild <ragnhildk98@gmail.com>
2019-07-01 10:37:28 +0200

    move task above question

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import TodoForm from './todoForm';
-
+import React from "react";
+import TodoForm from "./todoForm";
+import "../App.css";
 
 export default class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+    };
+    this.addTodo = this.addTodo.bind(this);
+    //this.mapTodos = this.mapTodos.bind(this);
+  }
 
-	constructor(props){
-		super(props);
-		this.state = {
-			todos: []
-		};
-		this.addTodo = this.addTodo.bind(this);
-		//this.mapTodos = this.mapTodos.bind(this);
-	}
-
-	addTodo(item){
-		this.setState(
-			{todos: [<li>{item}</li>, ...this.state.todos]}
-		);
-		console.log(this.state.todos)
-	};
-/*
+  addTodo(item) {
+    this.setState({ todos: [<li>{item}</li>, ...this.state.todos] });
+    console.log(this.state.todos);
+  }
+  /*
 	mapTodos(){
 		var i;
 		for (i = 0; i<this.state.todos.length; i++){
@@ -27,17 +24,13 @@ export default class TodoList extends React.Component {
 		}
 	}*/
 
-
-
-	render(){
-		return(
-			<div>
-				<div> Todo list app</div>
-				<TodoForm onAdd={this.addTodo}/>
-				<div>
-					{this.state.todos}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="todoApp">
+        <div> Todo app</div>
+        <TodoForm onAdd={this.addTodo} />
+        <div>{this.state.todos}</div>
+      </div>
+    );
+  }
 }
