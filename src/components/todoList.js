92c09a745a5907b50187574930319ff5d411375b
Andrine Lossius <andlossius@gmail.com>
2019-07-01 10:41:13 +0200

    add the ability to delete todos

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoForm from './todoForm';
+import TodoListItem from './todoListItem';
 
 
 export default class TodoList extends React.Component {
@@ -10,33 +11,37 @@ export default class TodoList extends React.Component {
 			todos: []
 		};
 		this.addTodo = this.addTodo.bind(this);
-		//this.mapTodos = this.mapTodos.bind(this);
 	}
 
 	addTodo(item){
+		if (item===''){alert("You need to add a todo");} 
+		else{
 		this.setState(
 			{todos: [<li>{item}</li>, ...this.state.todos]}
 		);
+		}
 		console.log(this.state.todos)
 	};
-/*
-	mapTodos(){
-		var i;
-		for (i = 0; i<this.state.todos.length; i++){
-			return <div>this.state.todos[i] </div>
-		}
-	}*/
 
+
+	deleteTodo(index) {
+		let todoArray = this.state.todos;
+		todoArray.splice(index,1);
+		this.setState({todos:todoArray})
+	}
 
 
 	render(){
+
+		let todos = this.state.todos.map((val,key) => {
+			return <TodoListItem key={key} text={val}
+				deleteMethod= { () => this.deleteTodo(key)}/>
+		})
 		return(
 			<div>
 				<div> Todo list app</div>
 				<TodoForm onAdd={this.addTodo}/>
-				<div>
-					{this.state.todos}
-				</div>
+				{todos}
 			</div>
 		);
 	}
--- src/components/todoListItem.js
+++ src/components/todoListItem.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 
-export default class todoListItem extends React.Component {
+class TodoListItem extends React.Component {
 
-	
+	render(){
+		return (
+			<div className="todoListItem" onClick={this.props.deleteMethod}>
+				{this.props.text}
+			</div>
+			);
+	}
 }
+
+export default TodoListItem;
