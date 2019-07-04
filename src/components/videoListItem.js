8492e2bdbc15a508f2d7bd2f20be2b2a79132e9d
Andrine Lossius <andlossius@gmail.com>
2019-07-04 09:07:01 +0200

    fix input field

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -1,14 +1,16 @@
 import React from "react";
+
+
 const VideoListItem = props => {
   const video = props.video;
   const onUserSelected = props.onUserSelected;
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li onClick={() => onUserSelected(video)} className="list-group-item">
+    <div onClick={() => onUserSelected(video)} >
       <img src={imageUrl} />
       <div>{video.snippet.title}</div>
-    </li>
+    </div>
   );
 };
 
