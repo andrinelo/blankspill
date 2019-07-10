bda379a540610f61fc461d90d2a683833d861a3c
Ragnhild <ragnhildk98@gmail.com>
2019-07-10 13:41:27 +0200

    change color on buttons when you hover over them

--- src/questions.js
+++ src/questions.js
@@ -4,15 +4,14 @@ import React from "react";
 var questions = [
   {
     q:
-      "Nå kan du ikke legge til varer i handlelista. Prøv selv ved å skrive inn noe du vil kjøpe og se hva som skjer 🚀 For å fikse dette - skriv ferdig funksjonen 𝘢𝘥𝘥𝘐𝘵𝘦𝘮. Tips: du kan legge til noe i en liste ved å skrive liste.push(item) ",
+      "Nå kan du ikke legge til varer i handlelista. Prøv selv ved å skrive inn noe du vil kjøpe og se hva som skjer 🚀 For å fikse dette - skriv ferdig funksjonen 𝘢𝘥𝘥𝘐𝘵𝘦𝘮. Tips: du kan legge til noe i en liste ved å skrive liste.push(item). ",
     startValue:
       "var shoppingList = [];\nfunction addItem(item){\n  //skriv din kode her\n  return shoppingList;\n}",
     test: function test(input) {
       let a = eval(input + "addItem('sitron')");
       if (a.toString) {
         if (
-          eval(input + "addItem('sitron')").toString() !==
-          ["sitron"].toString()
+          eval(input + "addItem('sitron')").toString() !== ["sitron"].toString()
         ) {
           return false;
         }
@@ -23,7 +22,7 @@ var questions = [
   },
   {
     q:
-      "Nå kan man legge til en 'tom vare', det er ikke helt heldig. Test det selv 🔍 Skriv ferdig metoden checkIfEmpty ved å fullføre en 'if-statement' for å rette opp i det ",
+      "Nå kan man legge til en 'tom vare', det er ikke helt heldig. Test det selv 🔍 Skriv ferdig metoden checkIfEmpty ved å fullføre en 'if-statement' for å rette opp i det.",
     startValue:
       "let shoppingList =[];\nfunction checkIfEmpty(item){\n  if (/*din kode her*/ === /*dine kode her*/){\n        return shoppingList;\n  }\n  else{\n    todoList.push(todoItem);\n    return todoList;\n  }\n}",
     test: function test(input) {
@@ -54,7 +53,7 @@ var questions = [
   },
   {
     q:
-      "I JavaScript har man noen innebygde funksjoner man kan bruke til forskjellige ting. Splice() til eksempel kan brukes for å endre en liste. KODEBLOKK: tall = [2,4,6,8]; Splice.tall(2,1) gir tall = [2,4,8] \n Nå kan man ikke slette todos, prøv selv 🔍 Skriv ferdig deleteTodo for å fikse det",
+      "I JavaScript har man noen innebygde funksjoner man kan bruke til forskjellige ting. Splice() til eksempel kan brukes for å endre en liste. KODEBLOKK: tall = [2,4,6,8]; Splice.tall(2,1) gir tall = [2,4,8] \n Nå kan man ikke slette todos, prøv selv 🔍 Skriv ferdig deleteTodo for å fikse det.",
     startValue:
       "function deleteItem(shoppingList, shoppingItem){\n  //skriv din kode her \n  return shoppingList;\n}",
     test: function test(input) {
@@ -82,28 +81,29 @@ var questions = [
       return false;
     },
   },
-  { 
-    q: "Man kan også gjøre regneoperasjoner. Si du ønsker å vite hvor mye alt på handlelista koster. Vi tar en snarvei og antar at hver vare koster 30kr. Skriv ferdig koden esimatePrice() for å regne ut et estimat på hva du må betale🧠", 
-    startValue: "function estimatePrice(shoppingListNumber){\n let priceForEachItem = 30;\n let totalCost = /*dine kode her*/;\n return totalCost +\n ' kroner for alt du skal ha';\n} ",
-    test: function test(input){
+  {
+    q:
+      "Man kan også gjøre regneoperasjoner. Si du ønsker å vite hvor mye alt på handlelista koster. Vi tar en snarvei og antar at hver vare koster 30kr. Skriv ferdig koden esimatePrice() for å regne ut et estimat på hva du må betale.🧠",
+    startValue:
+      "function estimatePrice(shoppingListNumber){\n let priceForEachItem = 30;\n let totalCost = /*dine kode her*/;\n return totalCost +\n ' kroner for alt du skal ha';\n} ",
+    test: function test(input) {
       let testNum = 5;
       let a = eval(input + "estimatePrice(testNum);");
-      if (a !== '150 kroner for alt du skal ha'){
+      if (a !== "150 kroner for alt du skal ha") {
         return false;
-        }
-      if (eval(input + "estimatePrice(1);") !== '30 kroner for alt du skal ha'){
+      }
+      if (
+        eval(input + "estimatePrice(1);") !== "30 kroner for alt du skal ha"
+      ) {
         return false;
-        }
+      }
       return true;
-
     },
   },
   {
     q: "Oppgave 5",
     startValue: "//meow",
-    test: function test(input){
-
-    },
+    test: function test(input) {},
   },
   {
     q: "Lag en funksjon med navn multiplication som ganger to tall",
